f4f0c2027acd029edcd49cbd2500be523e8f0105
Pawankumar7814 <pawank4285@gmail.com>
2023-07-10 22:55:54 +0530

    add validation while registeration of user

--- controllers/user_Controller.js
+++ controllers/user_Controller.js
@@ -1,57 +1,63 @@
 // Import models and modules
-const User = require('../model/user');
-const mongoose = require('mongoose');
+const User = require("../model/user");
+const mongoose = require("mongoose");
 
 // Controller for sign in page
 module.exports.signin = function(req, res) {
-    return res.status(200).render('users/signin', { title: " Sign In" });
-}
+    return res.status(200).render("users/signin", { title: " Sign In" });
+};
 
 // Controller for create session (Login the user)
 module.exports.createSession = function(req, res) {
-    return res.render('users/profile');
-}
+    return res.render("users/profile");
+};
 
 // Controller for sign up page
 module.exports.signup = function(req, res) {
-    return res.status(200).render('users/signup', { title: " Sign up" });
-}
+    return res.status(200).render("users/signup", { title: " Sign up" });
+};
 
 // Controller for create user
-module.exports.createUser = function(req, res) {
+module.exports.createUser = async function(req, res) {
+    var userData = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    };
     try {
-        var userData = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+        var checkUser = await User.findOne({ email: userData.email }); // Check the user if it is already exist then the if condition will be false
+        if (!checkUser) {
+            if (userData.password == req.body.confirmPassword) {
+                // Create user
+                User.create({
+                    name: userData.name,
+                    email: userData.email,
+                    password: userData.password,
+                });
+                return res.status(200).render("users/signin", { title: "Sign In" });
+            } else {
+                console.log(`Password must be same`);
+                return res.redirect("back");
+            }
         }
-        if (userData.password == req.body.confirmPassword) {
-            // Create user
-            User.create({
-                name: userData.name,
-                email: userData.email,
-                password: userData.password
-            });
-            return res.status(200).render('users/signin', { title: "Sign In" });
-        } else {
-            console.log(`Password must be same`);
-            return res.redirect('back');
-        }
+        console.log('User already exist');
+        return res.redirect("back");
     } catch (err) {
         console.log(`Error - ${err}`);
     }
-}
+};
 
 // Controller for Profile page
 module.exports.profile = function(req, res) {
-    return res.status(200).render('users/profile');
-}
-
+    return res.status(200).render("users/profile");
+};
 
 // Controller for logout
 module.exports.logout = function(req, res) {
     req.logout(function(err) {
-        if (err) { return next(err); }
-        res.redirect('/user/signin');
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/user/signin");
     });
-}
+};
